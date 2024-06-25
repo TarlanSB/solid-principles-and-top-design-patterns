2fa2204a44b9aebe86980647c36337a30e2185cf
TarlanSB <tarlanworkmail@gmail.com>
2024-06-26 00:17:52 +0300

    SRP

---
+++ src/main/java/solid/singleResponsibilityPrinciple/solution/SecurityService.java
@@ -0,0 +1,8 @@
+package solid.singleResponsibilityPrinciple.solution;
+
+public class SecurityService {
+    public boolean hasAccess (User user){
+        // logic to check if the user has access rights, ...
+        return true;
+    }
+}
