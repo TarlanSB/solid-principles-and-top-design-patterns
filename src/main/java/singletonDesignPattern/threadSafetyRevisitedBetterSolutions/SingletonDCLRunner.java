5bc602e7b3b4a08ea28071ab4fc17d77c8e61bff
TarlanSB <tarlanworkmail@gmail.com>
2024-07-26 23:20:37 +0300

    Thread-Safety Revisited - Better Solutions

---
+++ src/main/java/singletonDesignPattern/threadSafetyRevisitedBetterSolutions/SingletonDCLRunner.java
@@ -0,0 +1,33 @@
+package singletonDesignPattern.threadSafetyRevisitedBetterSolutions;
+
+public class SingletonDCLRunner {
+
+    public static void main(String[] args) {
+        SingletonDCL firstInstance = SingletonDCL.getInstance();
+        SingletonDCL secondInstance = SingletonDCL.getInstance();
+        System.out.println("First SingletonDCL instance hash: " + firstInstance.hashCode());
+        System.out.println("Second SingletonDCL instance hash: " + secondInstance.hashCode());
+    }
+}
+
+
+class SingletonDCL {
+    // volatile keyword ensures that multiple threads handle the uniqueInstance variable correctly
+    // when it is being initialized to the Singleton instance.
+    private volatile static SingletonDCL uniqueInstance;
+
+    private SingletonDCL() {
+        // private constructor to prevent instantiation
+    }
+
+    public static SingletonDCL getInstance() {
+        if (uniqueInstance == null) { // First check (no locking)
+            synchronized (SingletonDCL.class) {
+                if (uniqueInstance == null) { // Second check (with locking)
+                    uniqueInstance = new SingletonDCL();
+                }
+            }
+        }
+        return uniqueInstance;
+    }
+}
