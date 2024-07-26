baec99bf82a7a38eab05aa2f30456443ef3eae68
TarlanSB <tarlanworkmail@gmail.com>
2024-07-26 23:11:21 +0300

    Thread-Safety Revisited - Better Solutions

---
+++ src/main/java/singletonDesignPattern/threadSafetyRevisitedBetterSolutions/BillPughSingletonRunner.java
@@ -0,0 +1,32 @@
+package singletonDesignPattern.threadSafetyRevisitedBetterSolutions;
+
+public class BillPughSingletonRunner {
+
+    public static void main(String[] args) {
+        SingletonBillPugh firstInstance = SingletonBillPugh.getInstance();
+        SingletonBillPugh secondInstance = SingletonBillPugh.getInstance();
+        System.out.println("First SingletonBillPugh instance hash: " + firstInstance.hashCode());
+        System.out.println("Second SingletonBillPugh instance hash: " + secondInstance.hashCode());
+    }
+}
+
+class SingletonBillPugh {
+    
+    // Private constructor to prevent instantiation
+    private SingletonBillPugh() {
+    }
+
+    // Static inner class that contains the instance of the singleton class.
+    // When the SingletonBillPugh class is loaded, SingletonHelper class is not loaded into memory
+    // and only when someone calls the getInstance method, this class gets loaded and creates the Singleton instance.
+    private static class SingletonHelper {
+        private static final SingletonBillPugh INSTANCE = new SingletonBillPugh();
+    }
+
+    // Public static method that returns the instance of the class, this is the global access point
+    // for outer world to get the instance of the singleton class.
+    public static SingletonBillPugh getInstance() {
+        return SingletonHelper.INSTANCE;
+    }
+}
+
