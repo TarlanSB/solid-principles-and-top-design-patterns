faf7f07e3e59e1010a04e26e2c42bda3b3f28dac
TarlanSB <tarlanworkmail@gmail.com>
2024-07-27 22:33:58 +0300

    Architectural Considerations Part#2 - Singleton Attacks

---
+++ src/main/java/singletonDesignPattern/architecturalConsiderations2/CloningSingletonTest.java
@@ -0,0 +1,40 @@
+package singletonDesignPattern.architecturalConsiderations2;
+
+public class CloningSingletonTest {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        // Retrieve the singleton instance
+        ClonableSingleton instanceOne = ClonableSingleton.getInstance();
+        // Attempt to clone the singleton instance
+        ClonableSingleton instanceTwo = (ClonableSingleton) instanceOne.clone();
+
+        // Print out the hash codes of the original and cloned instance
+        // If cloning is not prevented properly, these could be different, demonstrating a flaw in singleton enforcement
+        System.out.println("Instance 1 hash:" + instanceOne.hashCode());
+        System.out.println("Instance 2 hash:" + instanceTwo.hashCode());
+    }
+}
+
+class ClonableSingleton implements Cloneable  {
+    // Static variable to hold the single instance of the class
+    private static ClonableSingleton instance;
+
+    // Private constructor to prevent instantiation from outside this class
+    private ClonableSingleton() {}
+
+    // Public method to provide global access to the singleton instance
+    public static ClonableSingleton getInstance() {
+        // Lazy initialization: if the instance doesn't exist, create it
+        if (instance == null) {
+            instance = new ClonableSingleton();
+        }
+        return instance;
+    }
+
+    // Override the Object clone method to allow cloning of the singleton instance
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return instance; // Return the same instance to enforce singleton
+    }
+    
+}
+
---
+++ src/main/java/singletonDesignPattern/architecturalConsiderations2/ReflectionSingletonTest.java
@@ -0,0 +1,35 @@
+package singletonDesignPattern.architecturalConsiderations2;
+
+import java.lang.reflect.Constructor;
+
+public class ReflectionSingletonTest {
+
+    public static void main(String[] args) {
+        // Get the singleton instance using the public getInstance method
+        Singleton instanceOne = Singleton.getInstance();
+        Singleton instanceTwo = null;
+
+        try {
+            // Get all declared constructors of the Singleton class
+            // Note: Even private constructors can be accessed with getDeclaredConstructors
+            Constructor[] constructors = Singleton.class.getDeclaredConstructors();
+            for (Constructor constructor : constructors) {
+                // Make the constructor accessible
+                constructor.setAccessible(true);
+                // Create a new instance of Singleton using the private constructor
+                instanceTwo = (Singleton) constructor.newInstance();
+                // Only need to break the singleton pattern once to prove it can be done
+                break;
+            }
+        } catch (Exception e) {
+            // Handle any exceptions that occur, such as IllegalAccessException or InstantiationException
+            e.printStackTrace();
+        }
+
+        // Print out the hash codes of the two instances
+        // If Singleton is properly implemented with defenses against reflection, these should be the same
+        System.out.println("Instance 1 hash:" + instanceOne.hashCode());
+        System.out.println("Instance 2 hash:" + instanceTwo.hashCode());
+    }
+}
+
